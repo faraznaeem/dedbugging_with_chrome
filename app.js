9f15f2231004c9e06f2c125e20a168dbb50b0e78
faraznaeem <faraznaeem@gmail.com>
2019-09-13 07:33:22 +0200

    Adds index and app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+function FizzBuzz() {
+    this.check = (number) => {
+        if (number % 15 === 0) {
+            return 'FizzBuzz';
+            debugger;
+        } else if (number % 5 === 0) {
+            return 'Buzz';
+        } else if (number % 3 === 0) {
+            return 'Fizz';
+        } else {
+            return number
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    let button = document.getElementById('button')
+    let displayDiv = document.getElementById('display_answer')
+    button.addEventListener('click', () =>{
+        let value = document.getElementById('value').value
+        let fizzBuzz = new FizzBuzz
+        let result = fizzBuzz.check(value)
+        displayDiv.innerHTML = result;
+    })
+})
